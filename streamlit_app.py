091b8bfc8ff885f9380b648abf964239e9a223b9
Edunbar103098 <87675821+Edunbar103098@users.noreply.github.com>
2021-07-20 09:25:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import reverse_geocode as rg
 import pprint
+import streamlit as st
 df = pd.read_csv("boston.csv")
 #street_map = gdp.read_file(r'C:\Users\Dunbar_Edwa\PycharmProjects\Boston Project\City_of_Boston_Boundary.shp')
 df = pd.read_csv("boston.csv")
@@ -18,3 +19,4 @@ chances_of_crime= (top_crime / (df["OFFENSE_DESCRIPTION"].count()))
 # df1 = df.drop(columns = ['INCIDENT_NUMBER','OFFENSE_CODE','OFFENSE_CODE_GROUP','OFFENSE_DESCRIPTION','DISTRICT','REPORTING_AREA','SHOOTING','OCCURRED_ON_DATE','YEAR','MONTH','DAY_OF_WEEK','HOUR','UCR_PART','Location'])
 day=df["DAY_OF_WEEK"].value_counts()
 day_percentages=(day/ (df["DAY_OF_WEEK"].count()))
+st.pie(top_crime)
