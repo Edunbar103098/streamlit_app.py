151a0164fc953ae3095491aa4d53a3760e860284
Edunbar103098 <87675821+Edunbar103098@users.noreply.github.com>
2021-07-20 09:23:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,20 @@
-import streamlit as st
+import csv
 import pandas as pd
-pd.read_csv("bostoncrime2021_7000_sample(1)")
+import matplotlib.pyplot as plt
+import reverse_geocode as rg
+import pprint
+df = pd.read_csv("boston.csv")
+#street_map = gdp.read_file(r'C:\Users\Dunbar_Edwa\PycharmProjects\Boston Project\City_of_Boston_Boundary.shp')
+df = pd.read_csv("boston.csv")
+all = df["OFFENSE_DESCRIPTION"].value_counts()
+crime_percentages= (all / (df["OFFENSE_DESCRIPTION"].count()))
+street=df["STREET"].value_counts()
+street_percentages=(street/ (df["STREET"].count()))
+street_top = street_percentages.head(n= 10)
+all = df["OFFENSE_DESCRIPTION"].value_counts()
+top_crime = all.head(n=10)
+chances_of_crime= (top_crime / (df["OFFENSE_DESCRIPTION"].count()))
+
+# df1 = df.drop(columns = ['INCIDENT_NUMBER','OFFENSE_CODE','OFFENSE_CODE_GROUP','OFFENSE_DESCRIPTION','DISTRICT','REPORTING_AREA','SHOOTING','OCCURRED_ON_DATE','YEAR','MONTH','DAY_OF_WEEK','HOUR','UCR_PART','Location'])
+day=df["DAY_OF_WEEK"].value_counts()
+day_percentages=(day/ (df["DAY_OF_WEEK"].count()))
