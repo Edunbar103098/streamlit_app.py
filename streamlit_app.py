5629dfad907b791fa80f214ab0738e76af8aa6e0
Edunbar103098 <87675821+Edunbar103098@users.noreply.github.com>
2021-07-20 10:15:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,5 +20,5 @@ import streamlit as st
 #day=df["DAY_OF_WEEK"].value_counts()
 #day_percentages=(day/ (df["DAY_OF_WEEK"].count()))
 #st.pie(top_crime)
-#st.button('Harder Daddy')
-st.first_app.py
+st.button('Harder Daddy')
+
