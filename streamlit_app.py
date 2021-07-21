47641b73af083efbaea9a810940ac237c011ac44
Edunbar103098 <87675821+Edunbar103098@users.noreply.github.com>
2021-07-21 16:09:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,18 @@
-#import csv
-#import pandas as pd
-#import matplotlib.pyplot as plt
-#import reverse_geocode as rg
-#import pprint
+import pandas as pd
+import geopandas
 import streamlit as st
-#df = pd.read_csv(r'C:\Users\Dunbar_Edwa\PycharmProjects\Boston Project\boston.csv')
-#street_map = gdp.read_file(r'C:\Users\Dunbar_Edwa\PycharmProjects\Boston Project\City_of_Boston_Boundary.shp')
-#df = pd.read_csv("boston.csv")
-#all = df["OFFENSE_DESCRIPTION"].value_counts()
-#crime_percentages= (all / (df["OFFENSE_DESCRIPTION"].count()))
-#street=df["STREET"].value_counts()
-#street_percentages=(street/ (df["STREET"].count()))
-#street_top = street_percentages.head(n= 10)
-#all = df["OFFENSE_DESCRIPTION"].value_counts()
-#top_crime = all.head(n=10)
-#chances_of_crime= (top_crime / (df["OFFENSE_DESCRIPTION"].count()))
 
-# df1 = df.drop(columns = ['INCIDENT_NUMBER','OFFENSE_CODE','OFFENSE_CODE_GROUP','OFFENSE_DESCRIPTION','DISTRICT','REPORTING_AREA','SHOOTING','OCCURRED_ON_DATE','YEAR','MONTH','DAY_OF_WEEK','HOUR','UCR_PART','Location'])
-#day=df["DAY_OF_WEEK"].value_counts()
-#day_percentages=(day/ (df["DAY_OF_WEEK"].count()))
-#st.pie(top_crime)
-a = st.sidebar.button('Harder Daddy')
+df = pd.DataFrame(
+    {'City': ['Buenos Aires', 'Brasilia', 'Santiago', 'Bogota', 'Caracas'],
+     'Country': ['Argentina', 'Brazil', 'Chile', 'Colombia', 'Venezuela'],
+     'Latitude': [-34.58, -15.78, -33.45, 4.60, 10.48],
+     'Longitude': [-58.66, -47.91, -70.66, -74.08, -66.86]})
+gdf = geopandas.GeoDataFrame(
+    df, geometry=geopandas.points_from_xy(df.Longitude, df.Latitude))
+st.write(gdf.head())
+world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+ax = world[world.continent == 'South America'].plot(
+    color='white', edgecolor='black')
+gdf.plot(ax=ax, color='red')
+st.pyplot()
 
