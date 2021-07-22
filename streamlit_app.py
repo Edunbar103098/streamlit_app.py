032705e6aed4b4ea6ea63bca57a3a9461802d488
Edunbar103098 <87675821+Edunbar103098@users.noreply.github.com>
2021-07-22 10:38:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,10 @@ st.sidebar.header('Boston Crime Statistics')
 st.sidebar.subheader('By: Edward Dunbar')
 page = st.sidebar.selectbox("Statistics", ["Gun Violence By District", "Deadly Days", "Top Crimes"]) 
 if page == "Gun Violence By District":
-  st.header('Gun Violence on the Rise During the Pandemic')
-  st.video('https://youtu.be/-OuII9QsdkI')
+  st.image('https://media-cldnry.s-nbcnews.com/image/upload/newscms/2021_15/3464361/210413-infrastructure-policing-2x1-cs.jpg')
   st.header('Gun Violence Vs Non Gun Violence, With District Crime Backdrop')
-  st.subheader('Black: Non Gun Violence')
-  st.subheader('Red: Gun Violence')
+  st.subheader('Black Dots: Non Gun Violence')
+  st.subheader('Red Dots: Gun Violence')
   st.image('https://i.pinimg.com/474x/84/37/cb/8437cb1783e45dffa657c3b11897b7aa.jpg')
   if st.button('Try This Code Yourself!'):
     link = '[Geopandas Chart](https://github.com/Edunbar103098/streamlit_app.py/blob/main/geopandas)'
@@ -21,7 +20,9 @@ if page == "Gun Violence By District":
   shooting = df["DISTRICT"].value_counts()
   st.write(shooting)
   st.header('Representation Of Crime by District')
-  st.image('https://i.pinimg.com/474x/dc/8e/3a/dc8e3a106cb1688df7f07e900ad158c4.jpg') 
+  st.image('https://i.pinimg.com/474x/dc/8e/3a/dc8e3a106cb1688df7f07e900ad158c4.jpg')
+  st.header('Gun Violence on the Rise During the Pandemic')
+  st.video('https://youtu.be/-OuII9QsdkI')
   if st.button('See Code to Create this Pie Chart:'):
     link = '[District Pie Chart](https://github.com/Edunbar103098/streamlit_app.py/blob/main/District%20Pie%20Chart)'
     st.markdown(link, unsafe_allow_html=True)          
@@ -46,6 +47,7 @@ elif page == "Top Crimes":
     st.image('https://massbaymovers.com/wp-content/uploads/2021/01/Boston-MA-Crime-Rate.jpg')
     all = df["OFFENSE_DESCRIPTION"].value_counts()
     top_crime = all.head(n=3)
+    st.header('Top 3 Crimes in Boston')
     st.write(top_crime)
     crime = st.selectbox("Pick a Crime", ['Investigate Person', 'Sick Assist', 'M/V - Leaving Scene- Property Damage'])
     if crime == "Investigate Person":
