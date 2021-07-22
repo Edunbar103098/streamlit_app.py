88c4d2ef741cb2f3c16b44c01720a7765b6fdce5
Edunbar103098 <87675821+Edunbar103098@users.noreply.github.com>
2021-07-21 21:00:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit as st
- 
+import webbrowser
 page = st.selectbox("Choose your Tab", ["Gun Violence", "Deadly Days", "Top Crimes"]) 
-#if page == "Gun Violence":
-    # Display details of page 1
+if page == "Gun Violence":
+  url = 'https://www.boston.gov/departments/police'
+  if st.button('Go to The Boston Police Homepage):
+     webbrowser.open_new_tab(url)             
 #elif page == "Deadly Days":
     # Display details of page 2
 #elif page == "Top Crimes":
+
 
