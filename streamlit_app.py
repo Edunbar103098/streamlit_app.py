4b872dac1c2f54b772b1c5fd0dda537a79c58738
Edunbar103098 <87675821+Edunbar103098@users.noreply.github.com>
2021-07-21 23:45:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ if page == "Gun Violence By District":
   st.subheader('Red: Gun Violence')
   st.image('https://i.pinimg.com/474x/84/37/cb/8437cb1783e45dffa657c3b11897b7aa.jpg')
   if st.button('See Code to Create this Chart:'):
-    link = '[Geopandas Chart]()'
+    link = '[Geopandas Chart](https://github.com/Edunbar103098/streamlit_app.py/blob/main/geopandas)'
     st.markdown(link, unsafe_allow_html=True)  
   if st.button('Go to The Boston Police Homepage'):
     link = '[City of Boston](https://www.boston.gov/departments/police)'
