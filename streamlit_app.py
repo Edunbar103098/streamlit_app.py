4d0012ed9dd229d890c39c63bdf010657dad9116
Edunbar103098 <87675821+Edunbar103098@users.noreply.github.com>
2021-07-21 22:28:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,10 @@ import streamlit as st
 import pandas as pd
 import csv
 df = pd.read_csv('https://raw.githubusercontent.com/Edunbar103098/streamlit_app.py/16786d8ea3dd570730347dabbecf5c8cc367a8fe/boston.csv')
-page = st.selectbox("Choose your Tab", ["Gun Violence", "Deadly Days", "Top Crimes"]) 
 st.image('https://www.ctvnews.ca/polopoly_fs/1.2386301.1613680608!/httpImage/image.jpg_gen/derivatives/landscape_1020/image.jpg')
+st.header('Boston Crime Statistics')
+st.subheader('By: Edward Dunbar')
+page = st.selectbox("Choose your Tab", ["Gun Violence", "Deadly Days", "Top Crimes"]) 
 if page == "Gun Violence":
   if st.button('Go to The Boston Police Homepage'):
     
