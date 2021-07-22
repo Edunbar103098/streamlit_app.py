bfdf53e61db41090de712ff73e578ccc30670e02
Edunbar103098 <87675821+Edunbar103098@users.noreply.github.com>
2021-07-21 22:26:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,10 @@ import pandas as pd
 import csv
 df = pd.read_csv('https://raw.githubusercontent.com/Edunbar103098/streamlit_app.py/16786d8ea3dd570730347dabbecf5c8cc367a8fe/boston.csv')
 page = st.selectbox("Choose your Tab", ["Gun Violence", "Deadly Days", "Top Crimes"]) 
+st.image(https://www.ctvnews.ca/polopoly_fs/1.2386301.1613680608!/httpImage/image.jpg_gen/derivatives/landscape_1020/image.jpg)
 if page == "Gun Violence":
   if st.button('Go to The Boston Police Homepage'):
-    st.image(r'C:\Users\Dunbar_Edwa\Dropbox\Desktop\New folderShootings.JPG')
+    
     link = '[City of Boston](https://www.boston.gov/departments/police)'
     st.markdown(link, unsafe_allow_html=True)           
 elif page == "Deadly Days":
