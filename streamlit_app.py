953e0b37836ca5cdf22f0a6d3895f470fda16216
Edunbar103098 <87675821+Edunbar103098@users.noreply.github.com>
2021-07-21 21:03:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,13 @@
+from bokeh.models.widgets import Div
 import streamlit as st
-import webbrowser
 page = st.selectbox("Choose your Tab", ["Gun Violence", "Deadly Days", "Top Crimes"]) 
 if page == "Gun Violence":
-  url = 'https://www.boston.gov/departments/police'
   if st.button('Go to The Boston Police Homepage'):
-     webbrowser.open_new_tab(url)             
+    js = "window.open('https://www.boston.gov/departments/police')"  # New tab or window
+    js = "window.location.href = 'https://www.boston.gov/departments/police'"  # Current tab
+    html = '<img src onerror="{}">'.format(js)
+    div = Div(text=html)
+    st.bokeh_chart(div)           
 #elif page == "Deadly Days":
     # Display details of page 2
 #elif page == "Top Crimes":
