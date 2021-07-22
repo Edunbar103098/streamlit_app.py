4929ea8bf4ae27be15639bed57c874288b22af47
Edunbar103098 <87675821+Edunbar103098@users.noreply.github.com>
2021-07-21 21:05:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ page = st.selectbox("Choose your Tab", ["Gun Violence", "Deadly Days", "Top Crim
 if page == "Gun Violence":
   if st.button('Go to The Boston Police Homepage'):
     link = '[City of Boston](https://www.boston.gov/departments/police)'
-    st.markdown(link, unsafe_allow_html=True))           
+    st.markdown(link, unsafe_allow_html=True)           
 #elif page == "Deadly Days":
     # Display details of page 2
 #elif page == "Top Crimes":
