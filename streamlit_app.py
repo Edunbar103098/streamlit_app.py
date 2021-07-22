ffb0ae93087ab84a16efb79049b5e7e36a8d671d
Edunbar103098 <87675821+Edunbar103098@users.noreply.github.com>
2021-07-21 23:14:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,13 @@ pip install plotly
 import streamlit as st
 import pandas as pd
 import csv
-import plotly.express as px
 df = pd.read_csv('https://raw.githubusercontent.com/Edunbar103098/streamlit_app.py/16786d8ea3dd570730347dabbecf5c8cc367a8fe/boston.csv')
 st.image('https://www.ctvnews.ca/polopoly_fs/1.2386301.1613680608!/httpImage/image.jpg_gen/derivatives/landscape_1020/image.jpg')
 st.header('Boston Crime Statistics')
 st.subheader('By: Edward Dunbar')
 page = st.selectbox("Statistics", ["Gun Violence By District", "Deadly Days", "Top Crimes"]) 
 if page == "Gun Violence By District":
-  if st.button('Go to The Boston Police Homepage'):
-    fig= px.pie(df, values='NUMBER', names='DAY_OF_WEEK')
-    st.plotly_chart(fig)
+  if st.button('Go to The Boston Police Homepage')
     link = '[City of Boston](https://www.boston.gov/departments/police)'
     st.markdown(link, unsafe_allow_html=True)           
 elif page == "Deadly Days":
