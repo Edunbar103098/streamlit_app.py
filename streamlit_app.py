bb7c2348a951101f7fed2822346327a8ac29d334
Edunbar103098 <87675821+Edunbar103098@users.noreply.github.com>
2021-07-22 00:10:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 import csv
 df = pd.read_csv('https://raw.githubusercontent.com/Edunbar103098/streamlit_app.py/16786d8ea3dd570730347dabbecf5c8cc367a8fe/boston.csv')
-st.image('https://www.ctvnews.ca/polopoly_fs/1.2386301.1613680608!/httpImage/image.jpg_gen/derivatives/landscape_1020/image.jpg')
+st.sidebar.image('https://www.ctvnews.ca/polopoly_fs/1.2386301.1613680608!/httpImage/image.jpg_gen/derivatives/landscape_1020/image.jpg')
 
-st.header('Boston Crime Statistics')
-st.subheader('By: Edward Dunbar')
+st.sidebar.header('Boston Crime Statistics')
+st.sidebar.subheader('By: Edward Dunbar')
 page = st.sidebar.selectbox("Statistics", ["Gun Violence By District", "Deadly Days", "Top Crimes"]) 
 if page == "Gun Violence By District":
   st.header('Gun Violence on the Rise During the Pandemic')
