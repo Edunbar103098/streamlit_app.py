6a87f3350feaf7eeef4df5cc83f4f5a8162aaf02
Edunbar103098 <87675821+Edunbar103098@users.noreply.github.com>
2021-07-21 23:30:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ if page == "Gun Violence By District":
   st.subheader('Red: Gun Violence')
   st.image('https://i.pinimg.com/474x/84/37/cb/8437cb1783e45dffa657c3b11897b7aa.jpg')
   if st.button('See Code to Create this Chart:'):
-      st.text('import pandas as pd
+      st.text('import pandas as pd'
               'import matplotlib.pyplot as plt'
 '# import descartes'
 '# import geopandas as gpd'
