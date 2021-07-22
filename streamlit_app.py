77f49856b5c73291c50eedcd2efdb884e8ba4093
Edunbar103098 <87675821+Edunbar103098@users.noreply.github.com>
2021-07-21 21:13:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import altair as alt
 import streamlit as st
 page = st.selectbox("Choose your Tab", ["Gun Violence", "Deadly Days", "Top Crimes"]) 
 if page == "Gun Violence":
+  st.image('Shootings.jpeg')
   if st.button('Go to The Boston Police Homepage'):
-    st.image(r'C:\Users\Dunbar_Edwa\PycharmProjects\Boston Project\Shootings.jpeg')
     link = '[City of Boston](https://www.boston.gov/departments/police)'
     st.markdown(link, unsafe_allow_html=True)           
 #elif page == "Deadly Days":
